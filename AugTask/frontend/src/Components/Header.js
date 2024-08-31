067a8e63f76e583bf9ad6d1812a8b7e1dbd3b1eb
Aravindswamymajjuri <aravindswamymajjuri143@gmail.com>
2024-08-31 12:37:39 +0530

    Add Header Component

---
+++ AugTask/frontend/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../Styles/Header.css"
+
+function Header() {
+  return (
+    <div className='head-container'>
+      <div className='first-color'>K-Hub React Project App</div>
+      <div className='second-color'></div>
+      <div className='third-color'></div>
+    </div>
+  );
+}
+
+export default Header;
